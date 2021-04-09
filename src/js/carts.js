e8cc290f98c0625a5a52c58e77c78d8ae447f71f
666lyj <747635824@qq.com>
2021-04-09 22:19:06 +0800

    20210409

---
+++ src/js/carts.js
@@ -0,0 +1,11 @@
+class Carts{
+    constructor(){
+        this.img = $('.product-img');
+        this.msg = $('.product-msg');
+        this.price = $('.product-price');
+        this.num_btn = $('product-count span');
+        this.num_text = $('num-text');
+        this.pay = $('product-pay span');
+    }
+
+}
